8616efd79145899d5d4d378e88911af78382c738
Maciek Kokot <mk@b-sharp.pl>
2021-06-13 19:43:58 +0200

    enc/dec not working for u and p

--- chapter1.py
+++ chapter1.py
@@ -1,5 +1,7 @@
 # Cracking Codes with Python
 # Chapter 1 notes and exercises
+import re
+
 
 caesars_nums = {1: "a", 2: 'b', 3: 'c', 4: 'd',
                 5: 'e', 6: 'f', 7: 'g', 8: 'h',
@@ -22,14 +24,14 @@ def caes_encode(plaintext, rot):
     rot = valid_rot(rot)
     output = []
     for char in plaintext:
-        if char in [' ', '.', ',', '?', '!']:
-            output.append(char)
-        else:
+        if re.match('[A-Za_z]', char):
             char_orig_num = list(caesars_nums.values()).index(char.lower())+1
             if char_orig_num+rot > 26:
                 output.append(caesars_nums[char_orig_num+rot-26])
             else:
                 output.append(caesars_nums[char_orig_num+rot])
+        else:
+            output.append(char)
     ciphertext = "".join(output)
     return ciphertext
 
@@ -38,13 +40,13 @@ def caes_decode(ciphertext, rot):
     rot = valid_rot(rot)
     output = []
     for char in ciphertext:
-        if char in [' ', '.', ',', '?', '!']:
-            output.append(char)
-        else:
+        if re.match('[A-Za-z]', char):
             char_orig_num = list(caesars_nums.values()).index(char.lower())+1
-            if char_orig_num-rot <= 0:
+            if char_orig_num-rot < 0:
                 output.append(caesars_nums[char_orig_num-rot+26])
             else:
                 output.append(caesars_nums[char_orig_num-rot])
+        else:
+            output.append(char)
     plaintext = "".join(output)
     return plaintext
